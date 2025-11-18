9df197e30c6506556ecf8a10c9c5531724cdfde1
jordanferreirasaran <jfsaran@gmail.com>
2025-11-18 15:06:36 -0300

    feat: create endpoints

--- settings/urls.py
+++ settings/urls.py
@@ -14,9 +14,79 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from django.contrib.auth.decorators import permission_required
+from django.urls import (
+    path,
+    include
+)
+from rest_framework import permissions
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="NXT API",
+        default_version='v1',
+        description="Documentação de reserva de salas de reunião",
+        terms_of_service="",
+        contact=openapi.Contact(email=""),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+    url=settings.SITE_URL_API
+)
+
+api_url_patterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("auth/", include("rest_framework.urls")),
+
+    path(
+        'swagger<format>/',
+        permission_required(
+            'permissions.IsAdminUser',
+            login_url="/admin/login"
+        )
+        (schema_view.without_ui(cache_timeout=0)),
+        name='schema-json',
+
+    ),
+    path(
+        'swagger/',
+        permission_required(
+            'permissions.IsAdminUser',
+            login_url="/admin/login"
+        )
+        (schema_view.with_ui(
+            'swagger',
+            cache_timeout=0
+        )),
+        name='schema-swagger-ui',
+
+    ),
+    path(
+        'redoc/',
+        permission_required(
+            'permissions.IsAdminUser',
+            login_url="/admin/login"
+        )
+        (schema_view.with_ui(
+            'redoc',
+            cache_timeout=0)),
+        name='schema-redoc'
+    ),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns.extend(api_url_patterns)
